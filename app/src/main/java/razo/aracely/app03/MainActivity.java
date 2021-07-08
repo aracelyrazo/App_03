08718dbcda8a997e87cbe6f3c7037f53cb5cfc39
aracelyrazo <razoracely7@gmail.com>
2021-07-07 21:45:01 -0500

    cclos de un proyecto

--- app/src/main/java/razo/aracely/app03/MainActivity.java
+++ app/src/main/java/razo/aracely/app03/MainActivity.java
@@ -3,6 +3,7 @@ package razo.aracely.app03;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,44 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(this,"Eventos: onCreate", Toast.LENGTH_LONG);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Toast.makeText(this, "Eventos: onStart", Toast.LENGTH_LONG);
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Toast.makeText(this, "Eventos: onStop", Toast.LENGTH_LONG);
+
+    }
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Toast.makeText(this, "Eventos: onResume", Toast.LENGTH_LONG);
+
+    }
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Toast.makeText(this, "Eventos: onDestroy", Toast.LENGTH_LONG);
+
+    }
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Toast.makeText(this, "Eventos: onRestart", Toast.LENGTH_LONG);
+
+    }
+
+    protected void onPause() {
+        super.onPause();
+        Toast.makeText(this, "Eventos: onPause", Toast.LENGTH_LONG);
+
     }
 }
